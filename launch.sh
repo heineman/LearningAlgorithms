44e2279330e48425afc91473fed589ab711c3335
George Heineman <heineman@wpi.edu>
2021-04-10 09:55:40 -0400

    set up launch.sh to run exercises as well and replacement unit tests

--- launch.sh
+++ launch.sh
@@ -8,6 +8,7 @@ PYTHON3=python3
 
 echo "Running test cases -- should take about 15 minutes."
 $PYTHON3 -m unittest discover > tests.txt
+$PYTHON3 ch07/replacement.py >> tests.txt
 
 echo "Generating figures and tables for the book. This will take about six hours"
 $PYTHON3 book.py > book.txt
@@ -20,3 +21,12 @@ $PYTHON3 ch04/timing.py > ch04.txt
 $PYTHON3 ch05/timing.py > ch05.txt
 $PYTHON3 ch06/timing.py > ch06.txt
 $PYTHON3 ch07/timing.py > ch07.txt
+
+echo "Challenge Exercise at end of each chapter."
+$PYTHON3 ch01/challenge.py > ch01.txt
+$PYTHON3 ch02/challenge.py > ch02.txt
+$PYTHON3 ch03/challenge.py > ch03.txt
+$PYTHON3 ch04/challenge.py > ch04.txt
+$PYTHON3 ch05/challenge.py > ch05.txt
+$PYTHON3 ch06/challenge.py > ch06.txt
+$PYTHON3 ch07/challenge.py > ch07.txt
